e893328c50f1314f87e7506862fd70296bf7a8d8
Raydan Bala <raydanb@gmail.com>
2021-08-16 20:58:50 -0700

    Added browserRouter, Route, and Link. Added links to Home, Posts, and Login/Register.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import Posts from './Posts'
+import Posts from './Posts';
 
 const App = () => {
   return <>
-    <h1>Title</h1>
-    <Posts />
+    <Link to='/'>Home</Link> | <Link to='/posts'>Posts</Link> | <Link to='/login'>Login/Register</Link>
+    <Route exact path='/'>
+      <h1>HomePage</h1>
+    </Route>
+    <Route path='/posts'>
+      <Posts />
+    </Route>
   </>
-}
+};
 
 ReactDOM.render(
-  <App />,
+  <Router><App /></Router>,
   document.getElementById('app'),
 );
