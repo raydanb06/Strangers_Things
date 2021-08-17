b79116fd0c9f755aafb2d22f62407a03522a8dd1
Raydan Bala <raydanb@gmail.com>
2021-08-16 20:43:26 -0700

    added Posts component, can fetch posts and display on page

---
+++ src/Posts.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+const Posts = () => {
+    const [posts, setPosts] = useState([]);
+    console.log('posts: ', posts);
+    
+    const fetchPosts = async () => {
+        await fetch('https://strangers-things.herokuapp.com/api/2105-SJS-RM-WEB-PT/posts')
+        .then(response => response.json())
+        .then(result => { setPosts(result.data.posts) })
+        .catch(console.error);
+    };
+
+    useEffect(() => {
+        fetchPosts()
+    }, []);
+
+    return <>
+        <h2 className='post-header'>Posts</h2>
+        {
+            posts.map((post, idx) => {
+                return <div key={idx} className='post'>
+                    {post.title}, {post.price}
+                </div>
+            })
+        }
+    </>
+}
+
+export default Posts;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Posts from './Posts'
+
 const App = () => {
-  return <div>
-    Hello World
-  </div>
+  return <>
+    <h1>Title</h1>
+    <Posts />
+  </>
 }
 
 ReactDOM.render(
