1c5e47a1b850ecbc46769e9fd1ce42704130787c
Raydan Bala <raydanb@gmail.com>
2021-08-16 21:12:34 -0700

    created api folder and components folder. Moved Posts.js and created index.js in components folder. Created index.js in api folder.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import Posts from './Posts';
+import { Posts } from './components';
 
 const App = () => {
   return <>
