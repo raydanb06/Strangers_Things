779992fc905ecf9f053c48931cd331240d78936f
Raydan Bala <raydanb@gmail.com>
2021-08-18 21:34:05 -0700

    Register and login complete and sets token in main app. Made Register its own component in Login component. Need to make Login its own component as well.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,26 @@
 import React, { useState } from 'react';
 
+import Register from './Register';
+
 const Login = (props) => {
   const { setToken } = props;
-
-  const [ newUsername, setNewUsername ] = useState('');
-  const [ newPassword, setNewPassword ] = useState('');
-  const [ newPasswordCheck, setNewPasswordCheck ] = useState('');
 
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(newUsername, typeof newUsername);
-    console.log(newPassword);
-    console.log(newPasswordCheck);
-    fetch('https://strangers-things.herokuapp.com/api/2105-SJS-RM-WEB-PT/users/register', {
+  const handleLoginSubmit = (ev) => {
+    ev.preventDefault();
+    console.log(username);
+    console.log(password);
+    fetch('https://strangers-things.herokuapp.com/api/2105-SJS-RM-WEB-PT/users/login', {
       method: "POST",
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         user: {
-          username: `${newUsername}`,
-          password: `${newPassword}`
+          username: `${username}`,
+          password: `${password}`
         }
       })
     })
@@ -33,27 +30,24 @@ const Login = (props) => {
       console.log(result);
     })
     .catch(console.error);
-    setNewUsername('');
-    setNewPassword('');
-    setNewPasswordCheck('');
-  }
+    setUsername('');
+    setPassword('');
+  };
 
   return <>
     <h1>LoginPage</h1>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor='new-username'>Username:</label>
-      <input placeholder='New Username' type='text' value={newUsername} onChange={
-        ev => setNewUsername(ev.target.value)
+    <Register />
+
+    <h2 className='login'>Login</h2>
+    <form onSubmit={handleLoginSubmit}>
+      <label htmlFor='username'>Username:</label>
+      <input placeholder='Username' type='text' value={username} onChange={
+        ev => setUsername(ev.target.value)
       }></input>
 
-      <label htmlFor='new-password'>Password:</label>
-      <input placeholder='New Password' type='password' /*minLength='8'*/ value={newPassword} onChange={
-        ev => setNewPassword(ev.target.value)
-      }></input>
-
-      <label htmlFor='new-password-check'>Re-enter Password:</label>
-      <input placeholder='Re-enter New Password' type='password' /*minLength='8'*/ value={newPasswordCheck} onChange={
-        ev => setNewPasswordCheck(ev.target.value)
+      <label htmlFor='password'>Password:</label>
+      <input placeholder='Password' type='password' /*minLength='8'*/ value={password} onChange={
+        ev => setPassword(ev.target.value)
       }></input>
 
       <button type='submit'>Submit</button>
